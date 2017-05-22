0dcd34283f2a683192a6053dcf07605bd1a20b24
Phasey <phasey.angus@gmail.com>
2017-05-22 12:34:35 +1000

    Dash script started, movemnet done

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 using UnityEngine.AI;
 
 
-public class EnemyMovement : MonoBehaviour {
-
-
-	Transform player;
-	NavMeshAgent nav;
-	public float MoveSpeed = 4;
-	public float MaxDist = 10;
-	public float MinDist = 5;
-
-//	void Awake (){
+//public class EnemyMovement : MonoBehaviour {
 //
-//		player = GameObject.FindGameObjectWithTag ("Player").transform;
-//		nav = GetComponent <NavMeshAgent> ();
 //
+//	Transform player;
+//	NavMeshAgent nav;
+//	public float MoveSpeed = 4;
+//	public float MaxDist = 10;
+//	public float MinDist = 5;
+//
+////	void Awake (){
+////
+////		player = GameObject.FindGameObjectWithTag ("Player").transform;
+////		nav = GetComponent <NavMeshAgent> ();
+////
+////	}
+//
+//	void Update () {
+//		//nav.SetDestination (player.position);
+//		transform.LookAt(player);
+//		if (Vector3.Distance(transform.position,player.position) > MinDist){
+//			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+//		}
+//
+//		if (Vector3.Distance (transform.position, player.position) < MaxDist) {
+//		}
 //	}
-
-	void Update () {
-		//nav.SetDestination (player.position);
-		transform.LookAt(player);
-		if (Vector3.Distance(transform.position,player.position) > MinDist){
-			transform.position += transform.forward * MoveSpeed * Time.deltaTime;
-		}
-
-		if (Vector3.Distance (transform.position, player.position) < MaxDist) {
-		}
-	}
-}
+//}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,17 @@ public class PlayerController : MonoBehaviour {
 	public List<GameObject> g = new List<GameObject> ();
 	public float movementSpeed = 60;
 	public float maxSpeed = 5;
-		public Rigidbody rigidBody;
+
+	public Vector3 dashDirection;
+	public float dashTime = 1f;
+	public float dashSpeed = 1f;
+	public float dashStopSpeed = 0.1f;
+
+	public float dashForce = 10;
+	public bool canDash = false;
+
+
+	public Rigidbody rigidBody;
 	public XboxController controller;
 
 	//Shooting
@@ -23,13 +33,19 @@ public class PlayerController : MonoBehaviour {
 
 	public Vector3 previousRotationDirextion = Vector3.forward;       
 
+	//private float currentDashTime;
+
+
 
 	void Start () {
 		rigidBody = GetComponent<Rigidbody> ();
+
+		//currentDashTime = dashTime;
 	}
 	void Update(){
 		RotatePlayer ();
-		FireGun ();
+		//FireGun ();
+		//DashPlayer();
 	}
 
 	private void RotatePlayer(){
@@ -52,32 +68,55 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate () {
 		MovePlayer ();
 	}
-	private void FireGun(){
-		if (XCI.GetAxis(XboxAxis.RightTrigger) > 0.15f){
-			if (Time.time - shootingTimer > timeBetweenShots) {
-				GameObject GO = Instantiate (bulletPrefab1, bulletSpawPosition1.position, Quaternion.identity) as GameObject;
-				GO.GetComponent<Rigidbody> ().AddForce(transform.forward * 20, ForceMode.Impulse);
-				Destroy(GO, 3);
-				shootingTimer = Time.time;
-			}
-		}
-	}
+//	private void FireGun(){
+//		if (XCI.GetAxis(XboxAxis.RightTrigger) > 0.15f){
+//			if (Time.time - shootingTimer > timeBetweenShots) {
+//				GameObject GO = Instantiate (bulletPrefab1, bulletSpawPosition1.position, Quaternion.identity) as GameObject;
+//				GO.GetComponent<Rigidbody> ().AddForce(transform.forward * 20, ForceMode.Impulse);
+//				Destroy(GO, 3);
+//				shootingTimer = Time.time;
+//			}
+//		}
 
 	private void MovePlayer(){
 
 		float axisX = XCI.GetAxis (XboxAxis.LeftStickX, controller);
 		float axisZ = XCI.GetAxis (XboxAxis.LeftStickY, controller);
+		float dash = XCI.GetAxis (XboxAxis.LeftTrigger, controller);
 		Vector3 movement = new Vector3 (axisX, 0, axisZ);
-		rigidBody.AddForce(movement * movementSpeed);
+		rigidBody.AddForce(movement * movementSpeed * dashSpeed);
 		if (rigidBody.velocity.magnitude > maxSpeed) {
-			rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;
+			rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;	
+		}
+		if(XCI.GetAxis(XboxAxis.LeftTrigger) >0.01f){
+			canDash =true;
+			dashSpeed = 10;
+		}else{
+			canDash = false;
+			dashSpeed= 1;
 		}
 	}
-}
 
 
 
 
+//	private void DashPlayer(){
+//
+//		float leftTrigger = XCI.GetAxis (XboxAxis.LeftTrigger, controller);
+//		if  (XboxAxis.LeftTrigger = true){
+//			GetComponent<Rigidbody> ().AddForce (transform.forward * dashForce, ForceMode.Impulse);
+//
+//		}
 
-
-
+//		float leftTrigger = XCI.GetAxis (XboxAxis.LeftTrigger, controller);
+//
+//		//if (XCI.GetAxis (XboxAxis.LeftTrigger) = ) {
+//			currentDashTime = 0.0f;
+//		//}
+//		if (currentDashTime < dashTime){
+//			dashDirection = new Vector3 (0, 0, dashSpeed);
+//			currentDashTime += dashStopSpeed;
+//		}//else{
+//		//	dashDirection = Vector3.zero;	
+//		//}
+	}
