d91cd8b1816be51c80af3bd168892b8e04a54918
Phasey <phasey.angus@gmail.com>
2017-05-29 15:55:32 +1000

    added those things to git

---
+++ Assets/Scripts/EnemySpawing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawing : MonoBehaviour {
+
+	public List<Transform> spawnPositionList = new List<Transform> ();
+	public List<GameObject> spawnRobotList = new List<GameObject> ();
+	public GameObject robotPrefab;
+
+
+	public float timeBetweenSpawns = 5f;
+	public float spawningTimer;
+
+
+	
+
+	void Update () {
+		SpawnEnemies ();
+	}
+
+	public void SpawnEnemies(){
+		Transform spawnPositionToUse = null;
+		GameObject robotToSpawn = null;
+		if (Time.time - spawningTimer > timeBetweenSpawns) {
+			spawnPositionToUse = spawnPositionList [Random.Range (0, spawnPositionList.Count)];
+			robotToSpawn = spawnRobotList [Random.Range (0, spawnRobotList.Count)];
+			Instantiate (robotPrefab, spawnPositionToUse.transform.position, Quaternion.identity);
+			spawningTimer = Time.time;
+		}
+	}
+}
