eeda70721f863a2edf283dca2a2055355df84ee4
Phasey <phasey.angus@gmail.com>
2017-05-30 16:40:41 +1000

    Adds the door to the program

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+	public GameObject doorToOpen;
+
+	public bool canOpen;
+	public float doorTimer = 2;
+
+	public float timeBeforeDoorOpens = 10f;
+
+	void Start () {
+		
+		doorToOpen.SetActive (true);
+	}
+
+	void Update () {
+		if (Time.time - doorTimer > timeBeforeDoorOpens) {
+			doorToOpen.SetActive (false);
+		}
+	}
+}
+
